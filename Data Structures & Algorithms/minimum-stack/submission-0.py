6d86ba306bec0d194674a8f455efaa9db7527dc3
lavlovesgit <148366150+lavlovesgit@users.noreply.github.com>
2026-08-13 19:51:32 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,27 @@
+from collections import deque
+class MinStack:
+
+    def __init__(self):
+        self.stack=deque()
+        
+    def push(self, val: int) -> None:
+         if not self.stack:
+            self.stack.append((val, val))
+         else:
+            current_min = min(val, self.stack[-1][1])
+            self.stack.append((val, current_min))
+        
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        
